744c59b049d1bfb4b8aa3d8b1a49171c4739feac
Sergio-stack10 <botobelexis@gmail.com>
2026-08-20 17:43:51 +0300

    Add files via upload

--- suivi_visite.py
+++ suivi_visite.py
@@ -1435,6 +1435,6 @@ with tab7:
 
 # --- SIGNATURE FIXEE EN BAS ---
 st.markdown(
-    "<div class='footer-fix'>Powerd by <span style='color: #25E2CC; font-weight: 700; letter-spacing: 1px; margin-left: 2px;'> TEAM TMM 🦄</span></div>", 
+    "<div class='footer-fix'>Developped by <span style='color: #25E2CC; font-weight: 700; letter-spacing: 1px; margin-left: 2px;'> TEAM TMM 🦄</span></div>", 
     unsafe_allow_html=True
 )
